b31c9fe0857911530c6fd62c91b84388cb3087b2
InJeKim97 <injaekim97@naver.com>
2022-10-26 02:42:11 +0900

    유효한 팰린드롬 (125. Valid Palindrome)

--- 125_Valid-Palindrome.py
+++ 125_Valid-Palindrome.py
@@ -8,3 +8,7 @@ class Solution:
                 res += i.lower()
 
         return res == res[::-1]
+
+# output (코드 제출시 해당 코드는 삭제)
+s = "A man, a plan, a canal: Panama"
+print(Solution().isPalindrome(s))
