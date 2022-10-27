660854c3150a70375019b5ac5bccab574954098e
InJeKim97 <injaekim97@naver.com>
2022-10-28 02:54:59 +0900

    유효한 팰린드롬 (125. Valid Palindrome)

--- 125_Valid-Palindrome.py
+++ 125_Valid-Palindrome.py
@@ -4,11 +4,11 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         res = ""
         for i in s:
-            if i.isalnum():  # Save only alphabetic characters
+            if i.isalnum(): # [a-zA-Z0-9]
                 res += i.lower()
 
         return res == res[::-1]
 
-# output (코드 제출시 해당 코드는 삭제)
+# input
 s = "A man, a plan, a canal: Panama"
 print(Solution().isPalindrome(s))
