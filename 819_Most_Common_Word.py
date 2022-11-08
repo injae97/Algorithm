4c52bf7d8b26f7594d68ea9021f54f2e489a86b5
InJeKim97 <injaekim97@naver.com>
2022-11-09 00:57:09 +0900

    가장 흔한 단어 (819. Most Common Word)

--- 819_Most_Common_Word.py
+++ 819_Most_Common_Word.py
@@ -23,3 +23,26 @@ print(Solution().mostCommonWord(paragraph, banned))
 
 
 
+import re
+import collections
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
+        """
+        :type paragraph: str, banned: List[str]
+        :rtype : str
+        """
+        # [^\w] : 단어 문자가 아닌 문자를 공백으로 치환 후 split() 공백 구분 e.g ['B', 'o', 'b', ' '] -> ['bob']
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+        .lower().split()
+            if word not in banned] # not in 을 통해 banned 값 포함X
+
+        counts = collections.defaultdict(int) # 개수를 담아두는 딕셔너리
+        for word in words:
+            counts[word] += 1
+
+        return max(counts, key=counts.get)
+
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+print(Solution().mostCommonWord(paragraph, banned))
+
