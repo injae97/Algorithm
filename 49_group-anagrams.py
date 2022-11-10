cf899c82e9f6919084fc709340709969f13c73bc
InJeKim97 <injaekim97@naver.com>
2022-11-10 22:12:18 +0900

    그룹 애너그램 (49. Group Anagrams)

--- 49_group-anagrams.py
+++ 49_group-anagrams.py
@@ -1,2 +1,18 @@
+import collections
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            """
+            sorted(word)  # ['a', 'e', 't']
+            ''.join(sorted(word)) # aet
+            [''.join(sorted(word))] # ['aet']            
+            """
+            # 문자 정렬 후 딕셔너리 추가
+            anagrams[''.join(sorted(word))].append(word)
+
+        return list(anagrams.values())
+
+# Input On LeetCode
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(Solution().groupAnagrams(strs))
