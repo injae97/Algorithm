baa8b0156ac29b05c75ed41f80e4edce8ea513e8
InJeKim97 <injaekim97@naver.com>
2022-11-15 20:54:33 +0900

    가장 긴 팰린드롬 부분 문자열 (5. Longest Palindromic Substring)

--- 5_Longest-Palindromic-Substring.py
+++ 5_Longest-Palindromic-Substring.py
@@ -1,2 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # Palindrome distinction & Two-Pointer Expansion
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        # Exception Handling
+        if len(s) < 2 or s == s[::-1]:
+            return s
+
+        res = ''
+        for i in range(0, len(s) - 1):
+            res = max(res,
+                      expand(i, i + 1),
+                      expand(i, i + 2),
+                      key=len)
+        return res
+
+s = "babad"
+print(Solution().longestPalindrome(s))
