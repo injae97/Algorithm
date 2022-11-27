c42690ea2b2304199bb2fe24eacbcde07446750e
InJeKim97 <injaekim97@naver.com>
2022-11-28 00:06:56 +0900

    세 수의 합 (15. 3Sum)

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -8,21 +8,25 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            for j in range(i + 1, len(nums) - 1):
-                # Duplicate Values Pass
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    # sum = 0 -> pass or skip
+                    res.append([nums[i], nums[left], nums[right]])
 
-                for k in range(j + 1, len(nums)):
-                    # Duplicate Values Pass
-                    if k > j + 1 and nums[k] == nums[k - 1]:
-                        continue
-
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        res.append([nums[i],nums[j],nums[k]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
 
         return res
-
 
 nums = [-1,0,1,2,-1,-4]
 print(Solution().threeSum(nums))
