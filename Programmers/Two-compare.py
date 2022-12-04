e7a6a4afc626d3df421183a28fc77ff29e3605d3
InJeKim97 <injaekim97@naver.com>
2022-12-05 00:45:03 +0900

    숫자 비교하기

---
+++ Programmers/Two-compare.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    if num1 == num2 :
+        return 1
+    elif num1 != num2 :
+        return -1
+
+# --------- input ------------
+num1, num2 = 2, 3
+print(solution(num1, num2))
