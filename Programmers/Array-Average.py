c276d91be56572453844f617a0f0d70bb9ee49a2
InJeKim97 <injaekim97@naver.com>
2022-12-05 04:24:37 +0900

    배열의 평균값

--- Programmers/Array-Average.py
+++ Programmers/Array-Average.py
@@ -3,8 +3,12 @@ def solution(numbers):
     for i in range(len(numbers)):
         temp += numbers[i]
 
-    answer = temp / len(numbers)
-    return answer
+    result = temp / len(numbers)
+    return result
+
+def solution2(numbers):
+    result = sum(numbers) / len(numbers)
+    return result
 
 # --------- input ------------
 numbers = [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
