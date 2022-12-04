cbe8a9e6f0c28ca62ce7219d5658b8153be63c85
InJeKim97 <injaekim97@naver.com>
2022-12-05 06:21:31 +0900

    머쓱이보다 키 큰 사람

--- Programmers/Array-Person.py
+++ Programmers/Array-Person.py
@@ -11,6 +11,7 @@ def solution2(array, height):
     array.append(height)
     array.sort(reverse=True) # 역순으로 값 정렬 [192, 180, 170, 167, 149]
     return array.index(height)
+
 # --------- input ------------
 array = [149, 180, 192, 170]
 height = 167
