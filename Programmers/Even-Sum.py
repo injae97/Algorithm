23bb2d7ebbac0253ad92df4ab2229327c3c3ce6c
InJeKim97 <injaekim97@naver.com>
2022-12-05 06:38:13 +0900

    짝수의 합

--- Programmers/Even-Sum.py
+++ Programmers/Even-Sum.py
@@ -1,10 +1,13 @@
 def solution(n):
     result = 0
-    for i in range(0, n+1):
+    for i in range(0, n + 1):
         if i % 2 == 0:
             result += i
 
     return result
+
+def solution2(n):
+    return sum([i for i in range(2, n + 1, 2)])
 
 # --------- input ------------
 n = 10
