93fbed405a148fc442316b75d60012ea8c1663d6
InJeKim97 <injaekim97@naver.com>
2022-12-05 17:06:05 +0900

    최댓값 만들기 (1)

--- Programmers/Array-Max(1).py
+++ Programmers/Array-Max(1).py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    numbers.sort(reverse=True)
+    numbers.sort(reverse=True) # numbers = sorted(numbers, reverse=True) : 변수에 역순으로 정렬 후 할당
     return numbers[0] * numbers[1]
 
 # --------- input ------------
