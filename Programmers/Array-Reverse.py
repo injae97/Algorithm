e283df4be3dd8da46cb6fe5866b7e9459737cd93
InJeKim97 <injaekim97@naver.com>
2022-12-05 14:07:26 +0900

    배열 뒤집기

--- Programmers/Array-Reverse.py
+++ Programmers/Array-Reverse.py
@@ -1,6 +1,10 @@
 def solution(num_list):
     return num_list[::-1]
 
+def solution2(num_list):
+    num_list.reverse()
+    return num_list
+
 # --------- input ------------
 num_list = [1, 2, 3, 4, 5]
 print(solution(num_list))
