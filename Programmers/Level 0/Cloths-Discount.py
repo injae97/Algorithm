c230ae5ca3679cf4dc6107760cf0e58994f5a938
InJeKim97 <injaekim97@naver.com>
2022-12-08 02:49:57 +0900

    옷가게 할인 받기

--- Programmers/Level 0/Cloths-Discount.py
+++ Programmers/Level 0/Cloths-Discount.py
@@ -1,16 +1,21 @@
 def solution(price):
-    if 0 <= price <= 99999:
-        return price
-
-    elif 100000 <= price <= 299999:
-        return int(price - (price * 0.05))
+    if 100000 <= price <= 299999:
+        price = price - (price * 0.05)
 
     elif 300000 <= price <= 499999:
-        return int(price - (price * 0.1))
+        price = price - (price * 0.1)
 
     elif 500000 <= price:
-        return int(price - (price * 0.2))
+        price = price - (price * 0.2)
+
+    return int(price)
+
+def solution2(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
 
 # --------- input ------------
 price = 150000
-print(solution(price))
+print(solution2(price))
