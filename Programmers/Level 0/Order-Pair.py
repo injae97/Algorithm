a3fe881d826e7733ab82c1dd3e033ce20460bbac
InJeKim97 <injaekim97@naver.com>
2022-12-12 13:58:56 +0900

    순서쌍의 개수

--- Programmers/Level 0/Order-Pair.py
+++ Programmers/Level 0/Order-Pair.py
@@ -1,10 +1,19 @@
 def solution(n):
     cnt = 0
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         if n % i == 0:
+            # print(f"({i}, {n//i})")      output : (1, 20), (2, 10), (4, 5), (5, 4), (10, 2), (20, 1)
             cnt += 1
 
     return cnt
+
+def solution2(n):
+    answer = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer.extend([(i, n//i)])
+
+    return len(answer)
 
 # --------- input ------------
 n = 20
