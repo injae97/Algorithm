7492ea1bc55d62b0a3a758fccdef2e1cb5597e66
InJeKim97 <injaekim97@naver.com>
2022-12-16 21:24:25 +0900

    문자열 정렬하기 (1)

--- Programmers/Level 0/String-Sort.py
+++ Programmers/Level 0/String-Sort.py
@@ -8,6 +8,9 @@ def solution(my_string):
     answer.sort()
     return answer
 
+def solution2(my_string):
+    return sorted([int(my_string[i]) for i in range(len(my_string)) if my_string[i].isdigit()])
+
 # --------- input ------------
 my_string = "hi12392"
 print(solution(my_string))
