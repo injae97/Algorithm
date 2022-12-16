31055f6daa500f2b2032d9e7cf181f04748f9d4e
InJeKim97 <injaekim97@naver.com>
2022-12-16 21:13:21 +0900

    n의 배수 고르기

---
+++ Programmers/Level 0/N-Multiple.py
@@ -0,0 +1,16 @@
+def solution(n, numlist):
+    answer = []
+    for i in range(len(numlist)):
+        if numlist[i] % n == 0:
+            answer.append(numlist[i])
+
+    return answer
+
+def solution2(n, numlist):
+    answer = [numlist[i] for i in range(len(numlist)) if numlist[i] % n == 0]
+    return answer
+
+# --------- input ------------
+n = 3
+numlist = [4, 5, 6, 7, 8, 9, 10, 11, 12]
+print(solution2(n, numlist))
