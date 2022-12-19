ed8d2bfb435711f0817a2721dfd8e2c332584694
InJeKim97 <injaekim97@naver.com>
2022-12-19 14:29:45 +0900

    배열 회전시키기

---
+++ Programmers/Level 0/Array-Rotation.py
@@ -0,0 +1,21 @@
+def solution(numbers, direction):
+    # pop keyword : Delete After Running
+    if direction == 'right':
+        numbers.insert(0, numbers.pop()) # pop() last index remove  e.g : [1, 2, 3] -> [1, 2]
+
+    elif direction == 'left':
+        numbers.append(numbers.pop(0)) # pop(0) first index remove  e.g : [1, 2, 3] -> [2, 3]
+
+    return numbers
+
+
+from collections import deque
+def solution2(numbers, direction):
+    numbers = deque(numbers)
+    numbers.rotate(1 if direction == 'right' else -1) # deque([3, 1, 2])
+    return list(numbers)
+
+# --------- input ------------
+numbers = [1, 2, 3]
+direction = "right"
+print(solution(numbers, direction))
