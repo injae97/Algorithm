1e06e73d405d048cb058d9dd9c93ed2e9521dbff
InJeKim97 <injaekim97@naver.com>
2022-12-21 03:22:36 +0900

    최댓값 만들기 (2)

---
+++ Programmers/Level 0/Array-Max(2).py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    numbers.sort()
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
+
+# --------- input ------------
+numbers = [0, -31, 24, 10, 1, 9]
+print(solution(numbers))
